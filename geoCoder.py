15e9d19eeea0fcee97abab31d836abbc99e87b0b
ichbinsisyphos <ichbinsisyphos@gmail.com>
2013-05-26 20:01:03 +0200

    simple lock file mechanism added for geoCoder.py

--- geoCoder.py
+++ geoCoder.py
@@ -3,14 +3,32 @@
 
 import geopy
 import sys
+import os
+import time
+
+#Pfad zum lockfile
+lockPath = "var/geoCoder.lock"
+
+#wenn lockfile existiert, 1 Sekunde warten und nochmal checken, sonst weiter
+while os.path.isfile(lockPath):
+	time.sleep(1)
+
+#lockfile erstellen
+open(lockPath,"w").close()
+
 
 returnString = "None"
 
 if len(sys.argv) == 2 and sys.argv[1]:
 	g = geopy.geocoders.GoogleV3()
-	locationList = [ result[0] + "%" + str(result[1][0]) + "%" + str(result[1][1]) for result in g.geocode(sys.argv[1], exactly_one=False) ]
+	locationList = [ result[0] + "%" + str(result[1][0]) + "%" \
+			+ str(result[1][1]) for result in g.geocode(sys.argv[1], \
+			exactly_one=False) ]
 
 	if len(locationList) > 0:
 		returnString = "$".join(locationList)
 
 sys.stdout.write(returnString)
+
+#Am Schluss lockfile loeschen
+os.remove(lockPath)
